854f52d5468f339f7acf0a283d22123518ea522e
adidevpanigrahi22-dev <adidevpanigrahi22@gmail.com>
2025-12-19 11:39:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ShowerHeadMic from "./App";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ShowerHeadMic />
+    <App />
   </React.StrictMode>
 );
